4d07fe9a335f14e45a3d57859928d62cec5d149f
momokan928 <kan.kan19980928@gmail.com>
2024-03-05 00:44:11 +0900

    2_Two Sum

---
+++ Arrays&Hashing/3_TwoSum.js
@@ -0,0 +1,38 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let answer = [];
+
+    for(let i = 0; i < nums.length -1; i++) {
+        for(let n = i+1; n < nums.length -1; n++) {
+            if (nums[i] + nums[n] == target) {
+                answer.push(i, n)
+            }
+        }
+    }
+
+    return answer;
+};
+
+/**
+ * 模範解答
+ */
+var modelAnswer = function(nums, target) {
+    const numberMap = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        const number = nums[i]
+
+        const difference = target - number
+
+        if (numberMap.has(difference)) return [numberMap.get(difference), i]
+        numberMap.set(number, i)
+    }
+}
